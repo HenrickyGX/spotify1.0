288464dd45c33f11e881637296d9dc9c61caf247
HenrickyGX <henrysampm007@outlook.com>
2023-05-25 12:16:46 -0400

    alteração

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,13 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\henry\\OneDrive\\Documentos\\youtube\\tailwind-spotify\\src\\app\\layout.tsx": [
+      "C:\\Users\\henry\\OneDrive\\Documentos\\youtube\\tailwind-spotify\\node_modules\\.pnpm\\next@13.4.3_react-dom@18.2.0_react@18.2.0\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\henry\\OneDrive\\Documentos\\youtube\\tailwind-spotify\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\henry\\OneDrive\\Documentos\\youtube\\tailwind-spotify\\src\\app\\page": [
+      "C:\\Users\\henry\\OneDrive\\Documentos\\youtube\\tailwind-spotify\\src\\app\\globals.css"
+    ]
+  }
+}
